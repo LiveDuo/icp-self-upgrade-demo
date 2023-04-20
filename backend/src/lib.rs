33974b80f2d8e4f57b967f9e279fd7ad5f514796
Andreas Tzionis <andy0n7@gmail.com>
2023-04-20 18:31:32 +0300

    cleanup

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,22 +1,21 @@
 use candid::{Principal, Encode};
 
-use ic_cdk::api::call::CallResult;
-
-use std::time::Duration;
-
 use ic_cdk::api::management_canister::main::*;
 
 async fn upgrade_canister_cb() {
-    ic_cdk::println!("Child: Self upgrading...");
-
+    ic_cdk::println!("upgrading");
+    
+    // get wasm
+    let wasm = b"\x00asm\x01\x00\x00\x00".to_vec();
+    // let wasm = include_bytes!("../../target/wasm32-unknown-unknown/release/child.wasm").to_vec();
+    
     // upgrade code
     let id = ic_cdk::id();
-    let wasm = b"\x00asm\x01\x00\x00\x00".to_vec();
     let install_args = InstallCodeArgument { mode: CanisterInstallMode::Upgrade, canister_id: id, wasm_module: wasm, arg: Encode!().unwrap(), };
-    let result: CallResult<()> = ic_cdk::api::call::call(Principal::management_canister(), "install_code", (install_args,),).await;
-    result.unwrap();
+    let _ = ic_cdk::api::call::call::<_, ()>(Principal::management_canister(), "install_code", (install_args,),).await.unwrap();
+    // let _ = ic_cdk::api::call::call::<_, ()>(Principal::management_canister(), "raw_rand", (),).await.unwrap();
 
-    ic_cdk::println!("Child: Upgrade done");
+    ic_cdk::println!("upgraded");
 }
 
 // dfx canister call aaaaa-aa update_settings '(record { canister_id = principal "'$(dfx canister id child)'"; settings = record { controllers = opt vec { principal "'$(dfx canister id child)'"; principal "'$(dfx identity get-principal)'"; }; }; })'
@@ -24,5 +23,5 @@ async fn upgrade_canister_cb() {
 #[ic_cdk_macros::update]
 async fn upgrade_canister() {
 
-    let _id = ic_cdk::timer::set_timer(Duration::from_millis(0), || ic_cdk::spawn(upgrade_canister_cb()));
+    upgrade_canister_cb().await;
 }
