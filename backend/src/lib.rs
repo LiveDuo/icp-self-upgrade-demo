53cf250f5dd30140cdececddae00922bc1ae3fe1
Andreas Tzionis <andy0n7@gmail.com>
2023-04-20 17:37:11 +0300

    first

---
+++ backend/src/lib.rs
@@ -0,0 +1,28 @@
+use candid::{Principal, CandidType, Deserialize, Encode};
+
+use ic_cdk::api::call::CallResult;
+
+use std::cell::RefCell;
+use std::time::Duration;
+
+use ic_cdk::api::management_canister::main::*;
+
+// static 
+
+async fn upgrade_canister_cb() {
+    ic_cdk::println!("Child: Self upgrading...");
+
+    // upgrade code
+    let id = ic_cdk::id();
+    let wasm = b"\x00asm\x01\x00\x00\x00".to_vec();
+    let install_args = InstallCodeArgument { mode: CanisterInstallMode::Upgrade, canister_id: id, wasm_module: wasm, arg: Encode!().unwrap(), };
+    let result: CallResult<()> = ic_cdk::api::call::call(Principal::management_canister(), "install_code", (install_args,),).await;
+    result.unwrap();
+}
+
+// dfx canister call child upgrade_canister
+#[ic_cdk_macros::update]
+async fn upgrade_canister() {
+
+    let _id = ic_cdk::timer::set_timer(Duration::from_millis(0), || ic_cdk::spawn(upgrade_canister_cb()));
+}
